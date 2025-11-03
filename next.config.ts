ebd736dd77def9f2926a4be413d86454f1096479
LordAizen1 <mohdkaif2003@gmail.com>
2025-11-04 00:22:35 +0530

    Initial commit: AI Document Agent project setup and features

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: 'C:\\Users\\mohdk\\Desktop\\dev_intern\\ai-doc-agent-nextjs\\ai-agent-nextjs',
+  },
   /* config options here */
 };
 
